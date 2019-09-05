7d4a7a5ccf89a0993f42730cd298618cae7e080a
Aracoix <aracoix@163.com>
2019-09-05 11:25:56 +0800

    新增bugly

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:bata_app_flutter_demo/flutter_density.dart';
 import 'package:bata_app_flutter_demo/splash_page.dart';
+import 'package:flutter_bugly/flutter_bugly.dart';
 
-void main() => /*runApp(SplashPage());*/
-FlutterDensity.ensureInitialized(360)
-..attachRootWidget(new SplashPage())
-..scheduleWarmUpFrame();
+void main() => FlutterBugly.postCatchedException((){
+  FlutterDensity.ensureInitialized(360)
+    ..attachRootWidget(new SplashPage())
+    ..scheduleWarmUpFrame();
+});
